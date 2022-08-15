8421892a69366ab71b0cbc66b71d4fe26f400e28
Rafisyah <m.arifin.putrasetia@gmail.com>
2022-08-15 11:04:54 +0700

    first-commit

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './footer.css'
+import {RiInstagramLine} from 'react-icons/ri'
+import {FaFacebookSquare} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
+import {BsYoutube} from 'react-icons/bs'
+import {SiBlogger} from 'react-icons/si'
+
+const footer = () => { 
+  return (
+    <footer id='footer'>
+      <a href='#' className='footer__logo'>M RAFISYAH R XI RPL A</a>
+
+      <div className='footer__social'>
+        <a href="https://www.instagram.com/rafisyah_07/" target="_blank"><RiInstagramLine/></a>
+        <a href="https://web.facebook.com/rafisyah.r" target="_blank"><FaFacebookSquare/></a>
+        <a href="https://mail.google.com/" target="_blank"><MdEmail/></a>
+        <a href="https://www.youtube.com/watch?v=a3Z7zEc7AXQ" target="_blank"><BsYoutube/></a>
+        <a href="https://rafisyahtugas.blogspot.com/" target="_blank"><SiBlogger/></a>
+      </div>
+
+      <div className='footer__kopirek'>
+        <small>&copy; Muhammad Rafisyah Rizkiyawan.<br/>All right reserved.</small>
+      </div>
+    </footer>
+  )
+}
+
+export default footer
